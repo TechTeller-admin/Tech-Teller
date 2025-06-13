96a8105f66a0655e8faac1c83e1533d890fad31e
TechTeller-admin <adewinner00@gmail.com>
2025-06-13 23:28:29 +0100

    Update backend_routes_main.py

--- backend_routes_main.py
+++ backend_routes_main.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from scraper import get_articles
 
 main_bp = Blueprint("main", __name__)
 
 @main_bp.route("/")
 def home():
-    return render_template("index.html")
+    articles = get_articles()
+    return render_template("index.html", articles=articles)
