0d4c15e0a5729ed8269bf3ad72104a5be0dff587
TechTeller-admin <adewinner00@gmail.com>
2025-06-13 23:51:47 +0100

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from backend_routes_main import main_bp
-
-app = Flask(__name__, template_folder='frontend_templates', static_folder='frontend_static')
-app.register_blueprint(main_bp)
-
-if __name__ == "__main__":
-    app.run(debug=True)
